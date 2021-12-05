39fcf2444ec816bc2fe3323c797573d40d22d873
Everson Cunha <eversonflp45@gmail.com>
2021-12-04 23:12:39 -0300

    atualizações

---
+++ javascript/app.js
@@ -0,0 +1,7 @@
+/*
+função de propaganda do ifood
+*/
+function slide1(){
+    document.getElementById('id').src="Img/propaganda.png";
+     document.getElementById('aId').href = "https://institucional.ifood.com.br/doacao" 
+ }
